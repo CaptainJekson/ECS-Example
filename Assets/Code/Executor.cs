1a6bcba6078e9c92f8ab8cbe10a5aea15a9cf11d
Evgeniy Skvortsov <evgeny@belka.app>
2022-06-24 00:25:42 +0300

    abstract Executor

---
+++ Assets/Code/Executor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Morpeh;
+using UnityEngine;
+using Zenject;
+
+namespace Code
+{
+    public abstract class Executor : IInitializable, ITickable, IFixedTickable, ILateTickable, IDisposable
+    {
+        private readonly World _world;
+        protected readonly SystemsGroup _initializers;
+        protected readonly SystemsGroup _updateSystems;
+        protected readonly SystemsGroup _fixedUpdateSystems;
+        protected readonly SystemsGroup _lateUpdateSystems;
+
+        protected Executor(World world)
+        {
+            _world = world;
+            
+            _initializers = _world.CreateSystemsGroup();
+            _updateSystems = _world.CreateSystemsGroup();
+            _fixedUpdateSystems = _world.CreateSystemsGroup();
+            _lateUpdateSystems = _world.CreateSystemsGroup();
+        }
+
+        public void Initialize()
+        {
+            _initializers.Initialize();
+            _updateSystems.Initialize();
+            _fixedUpdateSystems.Initialize();
+            _lateUpdateSystems.Initialize();
+        }
+        
+        public void Tick()
+        {
+            _updateSystems.Update(Time.deltaTime);
+        }
+
+        public void FixedTick()
+        {
+            _fixedUpdateSystems.FixedUpdate(Time.fixedDeltaTime);
+        }
+
+        public void LateTick()
+        {
+            _lateUpdateSystems.LateUpdate(Time.deltaTime);
+        }
+        
+        public void Dispose()
+        {
+            _initializers.Dispose();
+            _updateSystems.Dispose();
+            _fixedUpdateSystems.Dispose();
+            _lateUpdateSystems.Dispose();
+        }
+    }
+}
--- Assets/Code/Units/BallsSystemsExecutor.cs
+++ Assets/Code/Units/BallsSystemsExecutor.cs
@@ -1,66 +1,20 @@
-﻿using System;
-using Code.Units.BallUnit.Systems;
+﻿using Code.Units.BallUnit.Systems;
 using Morpeh;
-using UnityEngine;
-using Zenject;
 
 namespace Code.Units
 {
-    public class BallsSystemsExecutor : IInitializable, ITickable, IFixedTickable, ILateTickable, IDisposable
+    public sealed class BallsSystemsExecutor : Executor
     {
-        private readonly World _world;
-        private readonly SystemsGroup _initializers;
-        private readonly SystemsGroup _updateSystems;
-        private readonly SystemsGroup _fixedUpdateSystems;
-        private readonly SystemsGroup _lateUpdateSystems;
-        
-        public BallsSystemsExecutor(World world, CreateBallsInitializer сreateBallsInitializer, 
-            BallsCollisionSystem ballsCollisionSystem, BallDestroySystem ballDestroySystem, BallsMoveSystem ballsMoveSystem)
+        public BallsSystemsExecutor(World world, CreateBallsInitializer createBallsInitializer,
+            BallsCollisionSystem ballsCollisionSystem, BallDestroySystem ballDestroySystem,
+            BallsMoveSystem ballsMoveSystem) : base(world)
         {
-            _world = world;
-            
-            _initializers = _world.CreateSystemsGroup();
-            _updateSystems = _world.CreateSystemsGroup();
-            _fixedUpdateSystems = _world.CreateSystemsGroup();
-            _lateUpdateSystems = _world.CreateSystemsGroup();
-
-            _initializers.AddInitializer(сreateBallsInitializer);
-
-            _updateSystems.AddSystem(ballsCollisionSystem);
-            _updateSystems.AddSystem(ballDestroySystem);
-            
-            _fixedUpdateSystems.AddSystem(ballsMoveSystem);
-        }
-        
-        public void Initialize()
-        {
-            _initializers.Initialize();
-            _updateSystems.Initialize();
-            _fixedUpdateSystems.Initialize();
-            _lateUpdateSystems.Initialize();
-        }
-        
-        public void Tick()
-        {
-            _updateSystems.Update(Time.deltaTime);
-        }
-
-        public void FixedTick()
-        {
-            _fixedUpdateSystems.FixedUpdate(Time.fixedDeltaTime);
-        }
-
-        public void LateTick()
-        {
-            _lateUpdateSystems.LateUpdate(Time.deltaTime);
-        }
-        
-        public void Dispose()
-        {
-            _initializers.Dispose();
-            _updateSystems.Dispose();
-            _fixedUpdateSystems.Dispose();
-            _lateUpdateSystems.Dispose();
+            _initializers.AddInitializer(createBallsInitializer);         
+                                                  
+            _updateSystems.AddSystem(ballsCollisionSystem);   
+            _updateSystems.AddSystem(ballDestroySystem);      
+                                                  
+            _fixedUpdateSystems.AddSystem(ballsMoveSystem);   
         }
     }
 }
